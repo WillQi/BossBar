c8ff1b8d2326b9b0677608ef135f0804cb0ec6e0
WillQi <williamqipizza@gmail.com>
2020-02-15 17:23:15 -0700

    Adds @a option Used a actual IDE to formulate file structure xd

--- src/main/java/io/github/willqi/bossbar/commands/RemoveBossBarCommand.java
+++ src/main/java/io/github/willqi/bossbar/commands/RemoveBossBarCommand.java
@@ -5,8 +5,12 @@ import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.command.data.CommandParamType;
 import cn.nukkit.command.data.CommandParameter;
+import cn.nukkit.level.Level;
+import cn.nukkit.utils.DummyBossBar;
 import cn.nukkit.utils.TextFormat;
 import io.github.willqi.bossbar.BossBarManager;
+
+import java.util.Map;
 
 public class RemoveBossBarCommand extends Command {
 
@@ -37,18 +41,47 @@ public class RemoveBossBarCommand extends Command {
             return true;
         }
 
-        Player target = sender.getServer().getPlayer(args[0]);
-        if (target == null) {
-            sender.sendMessage("Usage: " + this.getUsage());
-            return true;
-        }
+        if (args[0].toLowerCase().equals("@a")) {
+            if (sender.isPlayer()) {
+                // Only for the level that user is on.
 
-        if (!BossBarManager.playerHasBossBar(target)) {
-            sender.sendMessage(target.getName() + " does not have any active boss bar!");
-            return true;
+                Map<Long, Player> players = sender.getServer().getPlayer(sender.getName()).getLevel().getPlayers();
+                for (Player player : players.values()) {
+
+                    if (BossBarManager.playerHasBossBar(player)) {
+                        BossBarManager.removeBossBar(player);
+                    }
+                }
+
+            } else {
+                // All levels.
+
+                Map<Integer, Level> levels = sender.getServer().getLevels();
+
+                for (Level level : levels.values()) {
+                    Map<Long, Player> players = level.getPlayers();
+                    for (Player player : players.values()) {
+                        if (BossBarManager.playerHasBossBar(player)) {
+                            BossBarManager.removeBossBar(player);
+                        }
+                    }
+                }
+
+            }
+        } else {
+            Player target = sender.getServer().getPlayer(args[0]);
+            if (target == null) {
+                sender.sendMessage("Usage: " + this.getUsage());
+                return true;
+            }
+
+            if (!BossBarManager.playerHasBossBar(target)) {
+                sender.sendMessage(target.getName() + " does not have any active boss bar!");
+                return true;
+            }
+            BossBarManager.removeBossBar(target);
+            sender.sendMessage(TextFormat.GREEN + "Removed boss bar for " + target.getName() + "!");
         }
-        BossBarManager.removeBossBar(target);
-        sender.sendMessage(TextFormat.GREEN + "Removed boss bar for " + target.getName() + "!");
 
         return true;
     }
--- src/main/java/io/github/willqi/bossbar/commands/SetBossBarCommand.java
+++ src/main/java/io/github/willqi/bossbar/commands/SetBossBarCommand.java
@@ -5,10 +5,13 @@ import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.command.data.CommandParamType;
 import cn.nukkit.command.data.CommandParameter;
+import cn.nukkit.level.Level;
 import cn.nukkit.utils.DummyBossBar;
 import cn.nukkit.utils.TextFormat;
 import cn.nukkit.utils.DummyBossBar.Builder;
 import io.github.willqi.bossbar.BossBarManager;
+
+import java.util.Map;
 
 public class SetBossBarCommand extends Command {
 
@@ -39,29 +42,72 @@ public class SetBossBarCommand extends Command {
             return true;
         }
 
-        Player target = sender.getServer().getPlayer(args[0]);
-        if (target == null) {
-            sender.sendMessage("Usage: " + this.getUsage());
-            return true;
-        }
-
         String bossBarText = args[1];
         for (int i = 2; i < args.length; i++) {
             bossBarText = bossBarText + " " + args[i];
         }
 
-        if (BossBarManager.playerHasBossBar(target)) {
-            DummyBossBar bossbar = BossBarManager.getPlayerBossBar(target);
-            bossbar.setText(bossBarText);
+        if (args[0].toLowerCase().equals("@a")) {
+
+            if (sender.isPlayer()) {
+                Map<Long, Player> players = sender.getServer().getPlayer(sender.getName()).getLevel().getPlayers();
+                for (Player player : players.values()) {
+
+                    if (BossBarManager.playerHasBossBar(player)) {
+                        DummyBossBar bossbar = BossBarManager.getPlayerBossBar(player);
+                        bossbar.setText(bossBarText);
+                    } else {
+                        DummyBossBar bossBar = new Builder(player)
+                            .text(bossBarText)
+                            .build();
+                        player.createBossBar(bossBar);
+                        BossBarManager.addBossBar(player, bossBar);
+                    }
+                }
+            } else {
+                // Send it to EVERYONE
+                Map<Integer, Level> levels = sender.getServer().getLevels();
+
+                for (Level level : levels.values()) {
+                    Map<Long, Player> players = level.getPlayers();
+                    for (Player player : players.values()) {
+                        if (BossBarManager.playerHasBossBar(player)) {
+                            DummyBossBar bossbar = BossBarManager.getPlayerBossBar(player);
+                            bossbar.setText(bossBarText);
+                        } else {
+                            DummyBossBar bossBar = new Builder(player)
+                                .text(bossBarText)
+                                .build();
+                            player.createBossBar(bossBar);
+                            BossBarManager.addBossBar(player, bossBar);
+                        }
+                    }
+                }
+            }
+
+            sender.sendMessage(TextFormat.GREEN + "Successfully set boss bar for everyone!");
+
         } else {
-            DummyBossBar bossBar = new Builder(target)
-            .text(bossBarText)
-            .build();
-            target.createBossBar(bossBar);
-            BossBarManager.addBossBar(target, bossBar);
+            Player target = sender.getServer().getPlayer(args[0]);
+            if (target == null) {
+                sender.sendMessage("Usage: " + this.getUsage());
+                return true;
+            }
+            if (BossBarManager.playerHasBossBar(target)) {
+                DummyBossBar bossbar = BossBarManager.getPlayerBossBar(target);
+                bossbar.setText(bossBarText);
+            } else {
+                DummyBossBar bossBar = new Builder(target)
+                        .text(bossBarText)
+                        .build();
+                target.createBossBar(bossBar);
+                BossBarManager.addBossBar(target, bossBar);
+            }
+
+            sender.sendMessage(TextFormat.GREEN + "Successfully set boss bar for " + target.getName() + "!");
         }
 
-        sender.sendMessage(TextFormat.GREEN + "Successfully set boss bar for " + target.getName() + "!");
+
 
         return true;
     }
